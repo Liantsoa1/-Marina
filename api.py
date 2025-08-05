e2a9217944cc7d1a5fc27c70444b27676f87ee6d
liantsoa <hei.liantsoa@gmail.com>
2025-08-05 11:38:18 +0300

    fix: set up host for render

--- api.py
+++ api.py
@@ -17,4 +17,5 @@ def solve():
         return jsonify({"error": e.output.decode()}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port
